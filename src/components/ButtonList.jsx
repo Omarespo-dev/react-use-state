745bf394e4d3c3d348a9718e452ee1d09b1f5925
Omarespo-dev <omaresposito21@gmail.com>
2025-02-17 19:03:44 +0100

    exercise complete

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,26 +1,41 @@
-// Importo array di ogetti
-import data from "../data/languages"
+// Importo array di oggetti
+import data from "../data/languages";
 
 // Importo react
-import React from "react"
+import React from "react";
 
-// Esporto funzione mappata 
+// Importo lo stato
+import { useState } from "react";
+
+// Esporto funzione mappata
 export default function ButtonList() {
-    
-    return (<>
+  
+    const [isOpen, setIsOpen] = useState(null); 
 
-        {data.map(lista =>
-            <React.Fragment key={lista.id}> 
-             <div className="button-set">
-                 <button><a href="#">{lista.title}</a></button>
-             </div>
-             <div className="content">
-                 <h4>{lista.title}</h4>
-                 <p>{lista.description}</p>
-             </div>
-         </React.Fragment>
-        )}
+  return (
+    <div className="container">
+      <div className="button-container">
+        {data.map((lista) => (
+          <div className="button-set" key={lista.id}>
+            <button onClick={() => setIsOpen(isOpen === lista.id ? null : lista.id)}>
+              {lista.title}
+            </button>
+          </div>
+        ))}
+      </div>
 
+      <div className="content-container">
+        {data.map((lista) => (
+          <div
+            className={`content ${isOpen === lista.id ? "active" : "none"}`}
+            key={lista.id}>
+                
+            <h4>{lista.title}</h4>
+            <p>{lista.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
-    </>)
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,5 @@ export default function Main() {
             <ButtonList />
             
         </main>
-
-        
     </>)
 }
